d1ec639c4221c045ac43bdd6b08dee031d7ca06a
Pratap Presolv <pratap@presolv360.com>
2025-02-16 04:52:35 +0530

    micro service base path config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,7 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1";
+export const API_URLS = {
+  AUTH_SERVICE:
+    process.env.NEXT_PUBLIC_AUTH_SERVICE_URL || "http://localhost:8000/api/v1",
+  NOTICE_SERVICE:
+    process.env.NEXT_PUBLIC_NOTICE_SERVICE_URL ||
+    "http://localhost:8001/api/v1",
+};
