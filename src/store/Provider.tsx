d66d8dde962fee15a552b7723383f033b3d065ce
Pratap Presolv <example@gmail.com>
2025-03-28 01:43:03 +0530

    Set the token in the Axios client for authenticated requests

--- src/store/Provider.tsx
+++ src/store/Provider.tsx
@@ -1,32 +1,34 @@
 "use client";
+
 import { store } from "@/store";
 import { Provider } from "react-redux";
 import { useEffect } from "react";
 import { useAppDispatch } from "@/store/hooks";
 import { setTokens } from "@/store/slices/authSlice";
+import { setAuthorizationHeader } from "@/services/apiClients/usersApiClient";
 
 function TokenInitializer() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     const cookies = document.cookie.split("; ");
-    console.log("All Cookies:", document.cookie);
     const accessToken = cookies
-      .find(row => row.startsWith("accessToken="))
+      .find((row) => row.startsWith("accessToken="))
       ?.split("=")[1];
+
     const refreshToken = cookies
-      .find(row => row.startsWith("refreshToken="))
+      .find((row) => row.startsWith("refreshToken="))
       ?.split("=")[1];
-    
-    console.log("Retrieved Tokens:", { accessToken, refreshToken }); 
+
     if (accessToken) {
-      dispatch(setTokens({ 
-        accessToken, 
-        refreshToken: refreshToken || "" 
-      }));
-      console.log("Initialized tokens in store:", { accessToken, refreshToken });
-    } else {
-      console.log("No access token found in cookies");
+      setAuthorizationHeader(accessToken);
+
+      dispatch(
+        setTokens({
+          accessToken,
+          refreshToken: refreshToken || "",
+        })
+      );
     }
   }, [dispatch]);
 
