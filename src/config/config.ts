2bbace6cba25f4a84b587cd43ad9898e3f5076e1
Pratap Presolv <example@gmail.com>
2025-04-04 10:49:41 +0530

    left menue updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,6 @@ export const API_URLS = {
   NOTICE_SERVICE:
     process.env.NEXT_PUBLIC_NOTICE_SERVICE_URL ||
     "https://16.170.157.110/api/v1",
-    USERS_SERVICE:
-    process.env.NEXT_PUBLIC_NOTICE_SERVICE_URL ||
-    "https://16.170.157.110/api/v1",
+  USERS_SERVICE:
+    process.env.NEXT_PUBLIC_AUTH_SERVICE_URL || "https://16.170.157.110/api/v1",
 };
